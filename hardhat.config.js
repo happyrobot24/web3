03a86c0fb1183ace8609fdd692761b25faf83495
paymon <paymon@safexapp.com>
2026-06-04 19:54:08 +0800

    7

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,8 @@ module.exports = {
   networks: {
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL,
-      accounts: [process.env.SEPOLIA_PRIVATE_KEY]
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY],
+      chainId: 11155111
     }
   },
   etherscan: {
