03b48665682f86f25f7a4216d64ba44e7b519448
paymon <paymon@safexapp.com>
2026-06-06 19:57:11 +0800

    8

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -126,7 +126,7 @@ contract FundMe {
         // 目标达成了，不能退款
         require(convertEth2USD(contractBalance) < TARGET_AMOUNT, "Target is reached");
         uint myContractAmount = funderToAmount[myAddress];
-        require(myContractAmount > 0, "you have no balance in this contract!");
+        require(myContractAmount > 0, "there is no fund for you");
 
         bool success;
         (success, ) = payable(myAddress).call{value: myContractAmount}("");
